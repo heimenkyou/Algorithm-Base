bc086fc6a5242c9003da8e9ea84449b5bab1a5df
heimenkyou <wenbin.lo@outlook.com>
2026-04-11 02:03:45 +0800

    refactor: P1305 新二叉树 - 用字典替换数组，优化麻烦的转换

--- Luogu/P1305 新二叉树/solution.py
+++ Luogu/P1305 新二叉树/solution.py
@@ -6,25 +6,22 @@ input = sys.stdin.readline
 
 def main():
     n = int(input())
-    left = [0] * 30
-    right = [0] * 30
+    left = {}
+    right = {}
     root = 0
     for i in range(n):
         # 转成字母表的顺序即 c='a'
-        node, l, r = [
-            ord(c) - ord('a') + 1 if c != '*' else c
-            for c in input().strip()
-        ]
+        node, l, r = input().strip()
         if l != '*': left[node] = l
         if r != '*': right[node] = r
         if i == 0: root = node
 
     res = []
     def preorder(root):
-        if root == 0: return
-        res.append(chr(root + ord('a') - 1))
-        preorder(left[root])
-        preorder(right[root])
+        if root == '*': return
+        res.append(root)
+        preorder(left.get(root, '*'))
+        preorder(right.get(root, '*'))
 
     preorder(root)
     print(''.join(res))
