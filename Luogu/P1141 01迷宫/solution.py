0fc1451c41de2384a2a7524eefb2c9264c6c39a0
heimenkyou <wenbin.lo@outlook.com>
2026-04-12 01:23:54 +0800

    feat: 【TLE】【练习DFS】P1141 01迷宫

--- Luogu/P1141 01迷宫/solution.py
+++ Luogu/P1141 01迷宫/solution.py
@@ -1,6 +1,7 @@
 # https://www.luogu.com.cn/problem/P1141
 
 import sys
+from collections import deque
 
 sys.setrecursionlimit(10**9)
 
@@ -16,32 +17,40 @@ def main():
     result = [[0] * n for _ in range(n)]
 
     dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    q = deque()
+    path = []
     # 返回从x,y出发，剩下未到达但能到达的格子数量。同时记录访问路径坐标
-    def dfs(x, y, path):
-        # 标记已访问过并记录坐标
-        result[x][y] = -1
-        path.append((x, y))
+    def bfs(i, j):
+        q.clear()
+        path.clear()
+        # 添加根节点
+        result[i][j] = -1
+        path.append((i, j))
+        q.append((i, j))
         cnt = 1
-        for d in dirs:
-            nx, ny = x + d[0], y + d[1]
-            if (0 <= nx < n and 0 <= ny < n
-                and result[nx][ny] == 0 and grid[nx][ny] != grid[x][y]):
-                cnt += dfs(nx, ny, path)
+        while q:
+            x, y = q.popleft()
+            for d in dirs:
+                nx, ny = x + d[0], y + d[1]
+                if (0 <= nx < n and 0 <= ny < n
+                    and result[nx][ny] == 0 and grid[nx][ny] != grid[x][y]):
+                    result[nx][ny] = -1
+                    q.append((nx, ny))
+                    path.append((nx, ny))
+                    cnt += 1
+        # 回填
+        for x, y in path:
+            result[x][y] = cnt
         return cnt
 
-    path = []
+    
     for _ in range(m):
         i, j = map(int, input().split())
         i, j = i - 1, j - 1
         if result[i][j] != 0:
             print(result[i][j])
         else:
-            path.clear()
-            cnt = dfs(i, j, path)
-            # 回填
-            for x, y in path:
-                result[x][y] = cnt
-            print(cnt)
+            print(bfs(i, j))
 
 if __name__ == '__main__':
     main()
