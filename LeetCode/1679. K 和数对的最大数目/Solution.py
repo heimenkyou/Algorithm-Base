be56c911ffda3c3e4cc4a686b16c924ccd0886c6
heimenkyou <wenbin.lo@outlook.com>
2026-04-20 14:47:48 +0800

    feat: 【一次遍历哈希表】1679. K 和数对的最大数目

--- LeetCode/1679. K 和数对的最大数目/Solution.py
+++ LeetCode/1679. K 和数对的最大数目/Solution.py
@@ -1,18 +1,17 @@
 # https://leetcode.cn/problems/max-number-of-k-sum-pairs/
 
-from collections import Counter
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         res = 0
-        cnt = Counter(nums)
+        cnt = defaultdict(int)
         for num in nums:
-            if (k - num == num and cnt[num] >= 2) or (
-                k - num != num and cnt[num] >= 1 and cnt[k - num] >= 1
-            ):
-                cnt[num] -= 1
+            if cnt[k - num] > 0:
                 cnt[k - num] -= 1
                 res += 1
+            else:
+                cnt[num] += 1
         return res
