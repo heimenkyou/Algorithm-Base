e2d4fce0575959730ec424368844e901f052ac10
heimenkyou <wenbin.lo@outlook.com>
2026-05-19 23:20:37 +0800

    refactor: 【哈希表】2540. 最小公共值

--- LeetCode/2540. 最小公共值/Solution.py
+++ LeetCode/2540. 最小公共值/Solution.py
@@ -6,5 +6,14 @@ from typing import List
 
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        r = set(nums1) & set(nums2)
-        return min(r) if len(r) > 0 else -1
+        s = set(nums1)
+        for num in nums2:
+            if num in s:
+                return num
+        return -1
+
+
+# class Solution:
+#     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+#         r = set(nums1) & set(nums2)
+#         return min(r) if len(r) > 0 else -1
